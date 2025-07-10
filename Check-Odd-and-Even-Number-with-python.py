fe1c5dd6e234914afdde2426f4980cc437b2e2dd
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-07-10 08:43:34 -0700

    Check-Odd-and-Even-Number-with-python
    1. At first, gets number.
    
    2. Use "if" & "else" with a special conditions.
    
    3. Output the results in two conditions.

---
+++ Check-Odd-and-Even-Number-with-python.py
@@ -0,0 +1,11 @@
+# At first, gets number.
+number = int(input("Please enter a number: "))
+
+# Use "if" & "else" with a special conditions.
+if number % 2 == 0:
+# Output the result in this condition.
+    print(f"{number} is even.")
+
+else:
+# Output the result in this condition.
+    print(f"{number} is odd.")
